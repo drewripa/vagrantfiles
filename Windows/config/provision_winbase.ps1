79cc47d32a4d7c6b42c420c1d56e04b93f64cc95
drewripa <ripa.andrew@gmail.com>
2018-02-14 15:36:30 +0200

    Devided lines, Fix typo

--- Windows/config/provision_winbase.ps1
+++ Windows/config/provision_winbase.ps1
@@ -1,1 +1,3 @@
-(Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Computername . | Where-Object {$_.IPAddress -like "192.168.0.2"}).SetGateway("192.168.0.1")
+$NIC = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Computername . |
+        Where-Object {$_.IPAddress -like "192.168.0.2"}
+$NIC.SetGateways("192.168.0.1")
