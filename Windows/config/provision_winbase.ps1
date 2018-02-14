89cb295fff710fb5fadb360b643c6f19516051d9
drewripa <ripa.andrew@gmail.com>
2018-02-14 15:35:02 +0200

    Onelined

--- Windows/config/provision_winbase.ps1
+++ Windows/config/provision_winbase.ps1
@@ -1,3 +1,1 @@
-$NIC = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Computername . |
-        Where-Object {$_.IPAddress -like "192.168.0.2"}
-$NIC.SetGateway("192.168.0.1")
+(Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Computername . | Where-Object {$_.IPAddress -like "192.168.0.2"}).SetGateway("192.168.0.1")
