a2d7744351d081d96cc636390b08b96faa01b0fa
drewripa <ripa.andrew@gmail.com>
2018-03-23 16:14:03 +0200

    Added additional libs

--- Linux/configs/provision_python3_tgbot.sh
+++ Linux/configs/provision_python3_tgbot.sh
@@ -5,7 +5,10 @@ echo "=== Updates ======="
 echo "==================="
 yum -y update
 
-yum -y install wget zlib-devel
+yum -y install wget zlib-devel install zlib-devel \
+              bzip2-devel openssl-devel ncurses-devel \
+              sqlite-devel readline-devel tk-devel gdbm-devel \
+              db4-devel libpcap-devel xz-devel
 yum -y groupinstall "Development tools"
 
 echo "==================="
@@ -15,11 +18,12 @@ if [ ! -f py3 ]; then
   wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz -O py3.tgz
   tar -xvzf py3.tgz
   cd Python-3.6.4
-  ./configure
-  make
-  make install
+  ./configure --enable-optimizations
+  make altinstall
   cd ..
   rm -rf Python-3.6.4
   rm -rf py3.tgz
   touch py3
 fi
+
+pipenv shell
