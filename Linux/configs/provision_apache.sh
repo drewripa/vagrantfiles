b53e6b995841627c1bd5892175a68418baba5fce
drewripa <ripa.andrew@gmail.com>
2018-03-29 09:52:14 +0300

    Added clone CV repo

--- Linux/configs/provision_apache.sh
+++ Linux/configs/provision_apache.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-echo "-------- Updates"
+echo "========================================================================="
+echo "========================== Updates ======================================"
+echo "========================================================================="
 yum -y update
 
-yum -y install httpd lynx iptables-services
+yum -y install httpd lynx iptables-services git
 iptables -I INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -I INPUT -i eth1 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 service iptables save
 service iptables restart
 
+echo "========================================================================="
+echo "========================== HTTPD service ================================"
+echo "========================================================================="
 systemctl start httpd
-
 systemctl enable httpd
 
-echo "-------- Index"
+echo "========================================================================="
+echo "========================== Clone Repo ==================================="
+echo "========================================================================="
 cd /var/www/html
-touch index.html
-echo 'Hello apache 99!' >> index.html
+git clone https://github.com/drewripa/cvwebpage.git
