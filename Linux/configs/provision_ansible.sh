d221a1da45730ec582abe5242e8562a526a19231
drewripa <ripa.andrew@gmail.com>
2018-05-29 10:22:51 +0300

    Rework ansible env export

--- Linux/configs/provision_ansible.sh
+++ Linux/configs/provision_ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ANSIBLECFGDIR="/etc/ansible"
 
 echo "========================================================================="
 echo "========================== Updates ======================================"
@@ -32,20 +33,31 @@ else
     echo "========================== Build Ansible Repo ==========================="
     echo "========================================================================="
     cd ./ansible
-    make rpm
-    rpm -Uvh ./rpm-build/ansible-*.noarch.rpm
+    pip install -r ./requirements.txt
+    source ./hacking/env-setup
+    # we need to manually export variables cose of vagrant using
+    echo "PATH=\$PATH:\$HOME/.local/bin:\$HOME/bin:\$HOME/ansible/bin:\$HOME/ansible/test/runner" >> /home/vagrant/.bashrc
+    echo "PYTHONPATH=\$HOME/ansible/lib:" >> /home/vagrant/.bashrc
+    echo "MANPATH=\$HOME/ansible/docs/man:" >> /home/vagrant/.bashrc
+    echo -e "export PATH\nexport PYTHONPATH\nexportMANPATH"
+    # make rpm
+    # rpm -Uvh ./rpm-build/ansible-*.noarch.rpm
 
     echo "========================================================================="
     echo "========================== Setup Defaults ==============================="
     echo "========================================================================="
+    if [! -d "$ANSIBLECFGDIR"]; then
+        mkdir $ANSIBLECFGDIR
+    fi
     echo "inventory      = /etc/ansible/hosts" >> /etc/ansible/ansible.cfg
     echo "sudo_user      = root" >> /etc/ansible/ansible.cfg
-    mv /etc/ansible/hosts /etc/ansible/hosts.origin
+    # commented due to absense hosts file in new release of self-builded ansible
+    #mv /etc/ansible/hosts /etc/ansible/hosts.origin
     touch /etc/ansible/hosts
 
     echo "========================================================================="
     echo -e "\n\n\tDon't forget to create user 'ansible'\n\tand add it into sudoers manually by visudo\n\n"
     echo -e "\tDon't forget to execute 'su ansible -' and 'ssh-keygen' for nopassword"
-    echo -e "\tDon't forget to execute 'ssh-copy-id ansible@yourhost\n\n'"
+    echo -e "\tDon't forget to execute 'ssh-copy-id ansible@yourhost\n\n"
     echo "========================================================================="
 fi
