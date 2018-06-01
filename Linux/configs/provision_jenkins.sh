f39b0d17537a11d720f3b47b6a5fa212e324b14f
drewripa <ripa.andrew@gmail.com>
2018-06-01 10:46:54 +0300

    Rework Jenkins provision

--- Linux/configs/provision_jenkins.sh
+++ Linux/configs/provision_jenkins.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+echo "========================================================================="
+echo "========================== Updates ======================================"
+echo "========================================================================="
 yum -y update
 yum -y upgrade
-
 yum -y install wget
 
-# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-#
-# yum install jenkins java-1.8.0-openjdk -y
-# systemctl start jenkins
 
-# echo "========================================================================="
-# cat /var/lib/jenkins/secrets/initialAdminPassword
-# echo "========================================================================="
+echo "========================================================================="
+echo "========================== Installing Jenkins ==========================="
+echo "========================================================================="
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
-yum install docker -y
+yum install jenkins java-1.8.0-openjdk -y
+systemctl enable jenkins
+systemctl start jenkins
+
+echo "========================================================================="
+echo "========================== Use admin password for login ================="
+echo "========================== to localhost:8080 ============================"
+cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "========================================================================="
